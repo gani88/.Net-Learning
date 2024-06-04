523bec8dae9632f48d93dba67d322a94ff6b37ae
zarekgema8 <zarekgema@gmail.com>
2024-06-04 21:39:07 +0700

    Initial commit for dotnet learning

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is for learning purpose
+
+Console.WriteLine("Welcome to my program using dotnet");
+
+Console.WriteLine("First, let me ask your name...");
+var yourName = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine($"{Environment.NewLine}Hello, {yourName}, and right now is at {currentDate}");
+Console.Write($"{Environment.NewLine}If you want to exit, just press any key..");
+
+Console.ReadKey(true);
